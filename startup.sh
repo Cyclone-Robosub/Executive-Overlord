1a9160fbd6755fb84cf1a1d9de6d62a93ceecc62
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-05-30 18:32:49 -0700

    setup

---
+++ startup.sh
@@ -0,0 +1,23 @@
+trap killgroup SIGINT
+
+killgroup(){
+    kill 0 
+}
+
+cd ../
+
+source /opt/ros/jazzy/setup.bash
+source install/setup.bash
+
+cd /Research_Cyclone/src
+colcon build &
+cd ../
+chmod +x startuppy.sh
+cd ../
+
+cd Thrust-Control
+cd ../
+
+cd StateSaver
+colcon build &
+cd ../
