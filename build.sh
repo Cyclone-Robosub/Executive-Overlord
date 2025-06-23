11b9e1f00137d24ff721d2e0df75cc93aa720a55
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-06-23 00:10:15 +0000

    Everything except the Thrust Control Build Works.

--- build.sh
+++ build.sh
@@ -3,37 +3,43 @@ trap killgroup SIGINT
 killgroup(){
     kill 0 
 }
+#NOTE : After the end of every section make sure the directory returns to the directory that this build script is located.
 
-
-
-cd ../ # Gets out of Exexecutive-Overload
 source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 
+#Start of Research build
 cd Research_Cyclone/src
 colcon build &
 cd ../
 chmod +x startuppy.sh
 cd ../
-cd Executive-Overlord/
+#End of Research Build 
 
-cd src/
-
+#Start of inertial sense build
 cd ros2_ws/src/
-colcon build &
+sudo ln -s inertial-sense-sdk/ROS/ros2
+colcon build 
 cd ../../
+#End of inertial sense build
 
+#Start of Thrust Control Build
+cd Thrust-Control
+colcon build & 
+cd ../
+#End of Thrust Control Build
 
-cd Thrust-Control/script
-./rmBuild.sh & 
-cd ../../
-
+#Start of CLTool build
 cd Robosub-CLTool/script
+chmod +x build.sh
 ./build.sh &
 cd ../../
+#End of CLTool build
 
+#Start of StateSaver build
 cd StateSaver
 colcon build &
 cd ../
+#End of StateSaver build
 
 wait
