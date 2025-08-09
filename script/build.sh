38c01fa143446811bf87a0c49099c3b0e6d6bc1b
zenyn <zenyn.ethridge@gmail.com>
2025-08-09 15:19:26 -0700

    renamed src to include for better accuracy

--- script/build.sh
+++ script/build.sh
@@ -1,61 +1,62 @@
-trap killgroup SIGINT
-
-killgroup(){
-    kill 0 
-}
-#NOTE : After the end of every section make sure the directory returns to the directory that this build script is located.
-export ROS_DOMAIN_ID=2
-source /opt/ros/jazzy/setup.bash
-source install/setup.bash
-
-cd ros2_ws/src
-
-#Start of StateSaver build
-cd StateSaver
-colcon build &
-cd ../
-#End of StateSaver build
-
-
-#Start of Research build
-cd Research_Cyclone/src
-colcon build &
-cd ../
-chmod +x startuppy.sh
-cd ../
-#End of Research Build 
-
-#Start of inertial sense build
-cd inertial-sense-sdk
-colcon build 
-cd ../
-cd ros2
-colcon build &
-cd ../
-#End of inertial sense build
-
-#Battery Management
-cd Battery_Management
-colcon build &
-cd ../
-
-
-#Start of Thrust Control Build
-cd Thrust-Control
-colcon build & 
-cd ../
-#End of Thrust Control Build
-
-#Start of CLTool build
-cd Robosub-CLTool/script
-chmod +x build.sh
-./build.sh &
-cd ../../
-#End of CLTool build
-
-#ExecutiveMainLoop
-cd ExecutiveMainLoop/scripts
-./build.sh & 
-cd ../..
-
-wait
+#trap killgroup SIGINT
+#
+#killgroup(){
+#    kill 0 
+#}
+##NOTE : After the end of every section make sure the directory returns to the directory that this build script is located.
+#export ROS_DOMAIN_ID=2
+#source /opt/ros/jazzy/setup.bash
+#source install/setup.bash
+#
+#cd include
+#
+##Start of StateSaver build
+#cd StateSaver
+#colcon build &
+#cd ../
+##End of StateSaver build
+#
+#
+##Start of Research build
+#cd Research_Cyclone/src
+#colcon build &
+#cd ../
+#chmod +x startuppy.sh
+#cd ../
+##End of Research Build 
+#
+##Start of inertial sense build
+#cd inertial-sense-sdk
+#colcon build 
+#cd ../
+#cd ros2
+#colcon build &
+#cd ../
+##End of inertial sense build
+#
+##Battery Management
+#cd Battery_Management
+#colcon build &
+#cd ../
+#
+#
+##Start of Thrust Control Build
+#cd Thrust-Control
+#colcon build & 
+#cd ../
+##End of Thrust Control Build
+#
+##Start of CLTool build
+#cd Robosub-CLTool/script
+#chmod +x build.sh
+#./build.sh &
+#cd ../../
+##End of CLTool build
+#
+##ExecutiveMainLoop
+#cd ExecutiveMainLoop/scripts
+#./build.sh & 
+#cd ../..
+#
+#wait
+#
