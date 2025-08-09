6105572f0a3d52e09968a343f1c06b46e99df39a
zenyn <zenyn.ethridge@gmail.com>
2025-08-09 15:31:02 -0700

    consolidated scripts

--- script/build.sh
+++ script/build.sh
@@ -1,62 +1,21 @@
-#trap killgroup SIGINT
-#
-#killgroup(){
-#    kill 0 
-#}
-##NOTE : After the end of every section make sure the directory returns to the directory that this build script is located.
-#export ROS_DOMAIN_ID=2
-#source /opt/ros/jazzy/setup.bash
-#source install/setup.bash
-#
-#cd include
-#
-##Start of StateSaver build
-#cd StateSaver
-#colcon build &
-#cd ../
-##End of StateSaver build
-#
-#
-##Start of Research build
-#cd Research_Cyclone/src
-#colcon build &
-#cd ../
-#chmod +x startuppy.sh
-#cd ../
-##End of Research Build 
-#
-##Start of inertial sense build
-#cd inertial-sense-sdk
-#colcon build 
-#cd ../
-#cd ros2
-#colcon build &
-#cd ../
-##End of inertial sense build
-#
-##Battery Management
-#cd Battery_Management
-#colcon build &
-#cd ../
-#
-#
-##Start of Thrust Control Build
-#cd Thrust-Control
-#colcon build & 
-#cd ../
-##End of Thrust Control Build
-#
-##Start of CLTool build
-#cd Robosub-CLTool/script
-#chmod +x build.sh
-#./build.sh &
-#cd ../../
-##End of CLTool build
-#
-##ExecutiveMainLoop
-#cd ExecutiveMainLoop/scripts
-#./build.sh & 
-#cd ../..
-#
-#wait
-#
+#!/bin/bash
+source /opt/ros/jazzy/setup.bash
+
+# source nucleus_drivers venv
+cd nucleus_driver/ros2
+source venv/bin/activate
+
+cd ../..
+
+colcon build \
+  --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  --packages-select  \
+  crs_dvl \
+  crs_ros2_interfaces \
+  interfaces \
+  nucleus_driver_ros2 \
+  thrust_control \
+  state_saver
+  # battery_management
+
+. install/setup.bash
