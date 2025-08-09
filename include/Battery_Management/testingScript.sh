87b311127c8d0cc07375ccc3a8d6385ea98011d9
zenyn <zenyn.ethridge@gmail.com>
2025-08-09 16:58:28 -0700

    added venv to gitignore

--- include/Battery_Management/testingScript.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
-
- source /opt/ros/jazzy/setup.bash
- colcon build --cmake-args -DBUILD_TESTING=ON
- cd build/battery_management
- ./battery_management_test > ../../Latest_gtest_output.txt
- 
--- include/crs_dvl/README.md
+++
@@ -1,2 +0,0 @@
-# crs_dvl
-crs code for interfacing with Nortek's ros2 driver node for the Nucleus 1000
