2ef86048222535adef76ddec7e2474cbf4580c0f
zenyn <zenyn.ethridge@gmail.com>
2025-08-10 21:13:43 -0700

    added run thrust control script

---
+++ script/run_thrust_control.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+trap killgroup SIGINT
+
+killgroup(){
+  kill $(jobs -p)
+  kill 0
+}
+
+ws_src="${PWD}/src"
+
+#NOTE : After the end of every section make sure the directory returns to the directory that this build script is located.
+# export ROS_DOMAIN_ID=2
+
+cd $ws_src
+pwd
+
+source /opt/ros/jazzy/setup.bash
+source install/setup.bash
+
+cd build/thrust_control
+./thrust_control &
+cd $ws_src
+
