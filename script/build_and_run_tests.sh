46eeffa4af463bf571354dcdef6c8a1db0968569
zenyn <zenyn.ethridge@gmail.com>
2025-08-10 17:53:53 -0700

    updated build_and_run_tests.sh

--- script/build_and_run_tests.sh
+++ script/build_and_run_tests.sh
@@ -7,19 +7,28 @@ killgroup(){
 
 #!/bin/bash
 source /opt/ros/jazzy/setup.bash
-
-colcon build \
-  --cmake-args -DBUILD_TESTING=ON -DMOCK_RPI=ON \
-  --packages-select  \
-  crs_dvl \
-  crs_ros2_interfaces \
-  interfaces \
-  nucleus_driver_ros2 \
-  thrust_control \
-  state_saver
+ colcon build \
+   --cmake-args -DBUILD_TESTING=ON -DMOCK_RPI=ON \
+   --packages-select  \
+     crs_dvl \
+     crs_ros2_interfaces \
+     interfaces \
+     nucleus_driver_ros2 \
+     thrust_control \
+     battery_management \
+     pwm_cltool \
+     state_saver
 
 . install/setup.bash
 
-colcon test --packages-skip nucleus_driver_ros2 interfaces
+colcon test \
+  --ctest-args \
+    -j 1 \
+  --packages-select  \
+    thrust_control \
+    battery_management \
+    pwm_cltool \
+    state_saver 
+   
 
 colcon test-result --all
